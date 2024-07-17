1cfda5d5756e76145d416f6463a3f7700e297baa
Utkarsh Shukla <utkarsh@github.com>
2024-07-17 16:59:23 +0530

    imported config for publisher

--- src/main/java/com/opsmx/messageservice/util/Publisher.java
+++ src/main/java/com/opsmx/messageservice/util/Publisher.java
@@ -1,10 +1,14 @@
 package com.opsmx.messageservice.util;
 
+import com.opsmx.messageservice.config.MessageServiceCamelConfig;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Import;
+
+@Import(MessageServiceCamelConfig.class)
 public class Publisher {
 
     @Autowired
