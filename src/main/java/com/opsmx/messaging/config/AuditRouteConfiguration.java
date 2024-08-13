a0b43e82650dc32c4ffdd4d54d34f96d45055178
Utkarsh Shukla <utkarsh@github.com>
2024-08-13 22:18:34 +0530

    removing interface role from configure function to get endpoint

--- src/main/java/com/opsmx/messaging/config/AuditRouteConfiguration.java
+++ src/main/java/com/opsmx/messaging/config/AuditRouteConfiguration.java
@@ -9,19 +9,19 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @ConditionalOnExpression("${message-broker.enabled:true}")
-@Import(MessageStarterCamelRouteConfig.class)
+@Import(MessageStarterRabbitMQConfig.class)
 public class AuditRouteConfiguration extends RouteBuilder {
 
 
     @Autowired
-    private MessageStarterCamelRouteConfig messageStarterCamelRouteConfig;
+    private MessageStarterRabbitMQConfig messageStarterRabbitMQConfig;
 
     @Override
     public void configure() throws Exception{
 
                 from(CamelConstants.camelAuditEndpoint)
                 .id(CamelConstants.auditQueue)
-                .to(messageStarterCamelRouteConfig.configure(CamelConstants.auditExchange, CamelConstants.auditQueueName))
+                .to(messageStarterRabbitMQConfig.configure(CamelConstants.auditExchange, CamelConstants.auditQueueName))
                 .end();
     }
 }
