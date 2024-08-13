72c233956b444fe620b75c22b9f1e8dbfe6af801
Utkarsh Shukla <utkarsh@github.com>
2024-08-13 13:05:39 +0530

    Audit Publisher config which gives audit publihser bean

---
+++ src/main/java/com/opsmx/messaging/util/AuditPublisherConfig.java
@@ -0,0 +1,45 @@
+package com.opsmx.messaging.util;
+
+import com.opsmx.messaging.config.MessageStarterCamelConfig;
+import com.opsmx.messaging.constants.CamelConstants;
+import com.opsmx.messaging.dto.AuditMessageDTO;
+import org.apache.camel.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import lombok.extern.slf4j.Slf4j;
+import com.google.gson.Gson;
+
+@Slf4j
+@Import(MessageStarterCamelConfig.class)
+@Configuration
+public class AuditPublisherConfig {
+
+    @Bean(name = "auditPublisher")
+    public AuditPublisher getAuditPublisher(){
+        return new AuditPublisher();
+    }
+
+    public class AuditPublisher {
+
+        @Autowired
+        private ProducerTemplate producerTemplate;
+
+        private Gson gson = new Gson();
+
+        public void sendToAudit(String resourceName, String resourceType, String action, String status, String entityName, String entityType, String message, String source, String cdTool) {
+
+            try {
+                AuditMessageDTO auditMessageDTO = new AuditMessageDTO(resourceName, resourceType, action, status, entityName, entityType, message, source, cdTool);
+                String serializedAuditMessage = gson.toJson(auditMessageDTO, AuditMessageDTO.class);
+                log.info("Publishing audit event to endpoint : {} the message: {}", CamelConstants.camelAuditEndpoint, serializedAuditMessage);
+                producerTemplate.sendBody(CamelConstants.camelAuditEndpoint, serializedAuditMessage);
+            } catch (Exception e) {
+                log.error("Error publishing audit event: {}", e);
+            }
+        }
+    }
+}
