047619810abefb6aab1e05600b7bbf6ca2e745c2
Utkarsh Shukla <utkarsh@github.com>
2024-08-30 22:06:13 +0530

    Revert "Imported configuration classes among configurations to resolve dependency issues."
    This reverts commit bb1066eb129e470aa50baceeea3f7d329988f2f6.

--- src/main/java/com/opsmx/messageservice/config/AuditRouteConfiguration.java
+++ src/main/java/com/opsmx/messageservice/config/AuditRouteConfiguration.java
@@ -5,11 +5,9 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @ConditionalOnExpression("${message-broker.enabled:true}")
-@Import(MessageServiceRabbitMQConfig.class)
 public class AuditRouteConfiguration extends RouteBuilder {
 
 
