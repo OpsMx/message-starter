ea97b1d61d0522c32c333cc297a8a756b7423eb5
Utkarsh Shukla <utkarsh@github.com>
2024-08-30 22:06:23 +0530

    Revert "Added configuration files for Apache Camel, RabbitMQ and also added a general consumer confgurer and a general publisher."
    This reverts commit 626a2c7adc8601f277c45eeec64c466304f17b1d.

--- src/main/java/com/opsmx/messageservice/config/CamelRouteConfig.java
+++
@@ -1,6 +0,0 @@
-package com.opsmx.messageservice.config;
-
-public interface CamelRouteConfig {
-
-    String configure(String exchange, String queueName);
-}
--- src/main/java/com/opsmx/messageservice/constants/CamelConstants.java
+++
@@ -1,6 +0,0 @@
-package com.opsmx.messageservice.constants;
-
-public interface CamelConstants {
-
-    String rabbitmq = "rabbitmq";
-}
