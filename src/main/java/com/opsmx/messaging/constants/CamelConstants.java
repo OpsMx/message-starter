d5ccb5644cfd5582b19386749cf214c2df900d7d
Utkarsh Shukla <utkarsh@github.com>
2024-08-30 21:52:02 +0530

    code cleanup

--- src/main/java/com/opsmx/messaging/constants/CamelConstants.java
+++ src/main/java/com/opsmx/messaging/constants/CamelConstants.java
@@ -3,9 +3,6 @@ package com.opsmx.messaging.constants;
 public interface CamelConstants {
 
     String rabbitmq = "rabbitmq";
-    String auditQueue = "audit-queue";
-    String auditQueueName = "audit-info";
-    String auditExchange = "audit.events";
     String camelAuditEndpoint = "direct:auditEvent";
 
 }
