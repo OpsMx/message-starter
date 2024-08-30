71e6055f3786210aa7bea63602bec728fd43bcb7
Utkarsh Shukla <utkarsh@github.com>
2024-08-30 22:05:47 +0530

    Revert "Changed AuditRoute to TestRoute to act as a default route for camel context to start"
    This reverts commit 2860eea9aead998d3fce4de892986939bae54ea7.

--- src/main/java/com/opsmx/messaging/config/AuditRouteConfiguration.java
+++ src/main/java/com/opsmx/messaging/config/AuditRouteConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Import;
 @ConditionalOnExpression("${message-broker.enabled:true}")
 @ComponentScan
 @Import(MessageStarterMessageBrokerProperties.class)
-public class TestRouteConfiguration extends RouteBuilder {
+public class AuditRouteConfiguration extends RouteBuilder {
 
     @Autowired
     private MessageStarterRabbitMQConfig messageStarterRabbitMQConfig;
@@ -21,11 +21,10 @@ public class TestRouteConfiguration extends RouteBuilder {
     @Override
     public void configure() throws Exception{
 
-//        from(CamelConstants.camelAuditEndpoint)
-//                .id(CamelConstants.auditQueue)
-//                .to(messageStarterRabbitMQConfig.configure(CamelConstants.auditExchange, CamelConstants.auditQueueName))
-//                .end();
-        from("direct:test").log("Test Route Configuration");
+        from(CamelConstants.camelAuditEndpoint)
+                .id(CamelConstants.auditQueue)
+                .to(messageStarterRabbitMQConfig.configure(CamelConstants.auditExchange, CamelConstants.auditQueueName))
+                .end();
     }
 
 }
