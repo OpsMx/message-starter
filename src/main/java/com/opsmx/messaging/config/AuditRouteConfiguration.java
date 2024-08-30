f8597382bae18905227992593dfefb1f4c1efa5a
Utkarsh Shukla <utkarsh@github.com>
2024-08-30 22:05:52 +0530

    Revert "TEst endpoints for camel"
    This reverts commit 4880f06b8158ee24c31625e27e353c6127b4f801.

--- src/main/java/com/opsmx/messaging/config/AuditRouteConfiguration.java
+++ src/main/java/com/opsmx/messaging/config/AuditRouteConfiguration.java
@@ -29,7 +29,7 @@ public class AuditRouteConfiguration extends RouteBuilder {
 //                .id(CamelConstants.auditQueue)
 //                .to(getRabbitMQEndPoint(CamelConstants.auditExchange, CamelConstants.auditQueueName))
 //                .end();
-        from("direct:test").log("Test Route Configuration");
+        from("").to("").end();
     }
 
 //    public String getRabbitMQEndPoint(String exchange, String queueName){
