2ec9269d917a58e8185b2fa5a03404cabe329e43
Utkarsh Shukla <utkarsh@github.com>
2024-08-30 22:05:47 +0530

    Revert "code cleanup"
    This reverts commit 9f0f4925b83ab2faa51428c721e869c733a77308.

--- src/main/java/com/opsmx/messaging/config/TestRouteConfiguration.java
+++ src/main/java/com/opsmx/messaging/config/TestRouteConfiguration.java
@@ -1,15 +1,30 @@
 package com.opsmx.messaging.config;
 
 
+import com.opsmx.messaging.constants.CamelConstants;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ConditionalOnExpression("${message-broker.enabled:true}")
+//@ComponentScan
+//@Import(MessageStarterMessageBrokerProperties.class)
 public class TestRouteConfiguration extends RouteBuilder {
+//
+//    @Autowired
+//    private MessageStarterRabbitMQConfig messageStarterRabbitMQConfig;
+
     @Override
     public void configure() throws Exception{
+
+//        from(CamelConstants.camelAuditEndpoint)
+//                .id(CamelConstants.auditQueue)
+//                .to(messageStarterRabbitMQConfig.configure(CamelConstants.auditExchange, CamelConstants.auditQueueName))
+//                .end();
         from("direct:test").log("Test Route Configuration");
     }
 
