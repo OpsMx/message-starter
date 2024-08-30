a738779c4fd04b20e704c6e2902f08a5825f2361
Utkarsh Shukla <utkarsh@github.com>
2024-08-30 22:05:50 +0530

    Revert "OP-21637: cleanup"
    This reverts commit 942cec036bf3c8fc458b2278caf44f6dad5c90b0.

--- src/main/java/com/opsmx/messaging/config/MessageStarterRabbitMQConfig.java
+++ src/main/java/com/opsmx/messaging/config/MessageStarterRabbitMQConfig.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ConditionalOnExpression("${message-broker.enabled:true}")
 @ConditionalOnProperty(value = "message-broker.endpoint.name", havingValue = CamelConstants.rabbitmq)
+//@Import(MessageStarterMessageBrokerProperties.class)
+//@ComponentScan
 public class MessageStarterRabbitMQConfig implements MessageStarterCamelRouteConfig {
 
     @Autowired
